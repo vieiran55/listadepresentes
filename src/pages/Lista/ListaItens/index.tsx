e00f0564988a9d6e2b46b9971e684abe293fdb4f
Antonio Vieira <vieira.n55@gmail.com>
2023-04-19 18:22:36 -0300

    lista atualizada

--- src/pages/Lista/ListaItens/index.tsx
+++ src/pages/Lista/ListaItens/index.tsx
@@ -1,35 +1,61 @@
 import { Link } from "react-router-dom";
 import estilos from "./ListaItens.module.scss";
 import listaDeCompras from "../../../dados/listadepresentes.json";
+import { useEffect, useState } from "react";
 
 interface Props {
   id: number;
   title: string;
   link: string;
   photo: string;
+  status: string;
   price: number;
 }
 
 export default function ListaItens(props: Props) {
-  const { id, title, link, photo, price } = props;
+  const { id, title, link, photo, price, status } = props;
   const pix = "https://nubank.com.br/pagar/xw2h0/YToiVhZ4ZT";
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    if (status === "disponivel") {
+      setIsShown(true);
+    }
+  }, []);
 
   return (
-    <div className={estilos.corpo__card}>
+    <div className={estilos[`corpo__${status}`]}>
+      <span></span>
       <li className={estilos.corpo__lista__item}>
-        <h1 className={estilos.corpo__lista__item__titulo}>{title.substring(0,30)}</h1>
-        <img className={estilos.corpo__lista__item__imagem} src={photo} alt={title}/>
+        <h1 className={estilos.corpo__lista__item__titulo}>
+          {title.substring(0, 30)}
+        </h1>
+        <img
+          className={estilos.corpo__lista__item__imagem}
+          src={photo}
+          alt={title}
+        />
+        {isShown &&
         <h2 className={estilos.corpo__lista__item__preco}>{`R$ ${price}`}</h2>
-        <div className={estilos.corpo__lista__item__acoes}>
-          <Link className={estilos.corpo__lista__item__acoes__link} to={link}>
-            Quero Comprar e entregar para os noivos
-          </Link>
-          <Link className={estilos.corpo__lista__item__acoes__pix} to={pix}>
-            Quero Fazer o Pix do valor para que os noivos comprem
-          </Link>
-        </div>
-        <Link to={"https://form.respondi.app/LdirwZxI"} className={estilos.corpo__lista__item__confirmar}>Confirmar
+        }
+        {isShown && (
+          <div className={estilos.corpo__lista__item__acoes}>
+            <Link className={estilos.corpo__lista__item__acoes__link} to={link}>
+              Quero Comprar e entregar para os noivos
+            </Link>
+            <Link className={estilos.corpo__lista__item__acoes__pix} to={pix}>
+              Quero Fazer o Pix do valor para que os noivos comprem
+            </Link>
+          </div>
+        )}
+        {isShown &&
+        <Link
+          to={"https://form.respondi.app/LdirwZxI"}
+          className={estilos.corpo__lista__item__confirmar}
+        >
+          Vou dar esse presente
         </Link>
+        }
       </li>
     </div>
   );
--- src/pages/Lista/index.tsx
+++ src/pages/Lista/index.tsx
@@ -2,19 +2,30 @@ import { Link } from "react-router-dom";
 import estilos from "./Lista.module.scss";
 import listaDeCompras from "../../dados/listadepresentes.json";
 import ListaItens from "./ListaItens";
+import { BsArrowUpCircleFill } from "react-icons/bs";
 
 export default function Lista() {
+
+  const topo = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className={estilos.corpo}>
-      <h1 className={estilos.corpo__titulo}>Lista de Presentes</h1>
+      <h1 className={estilos.corpo__casal}>Antonio e Gabriela</h1>
       <ol className={estilos.corpo__lista}>
+        <h2 className={estilos.corpo__titulo}>Lista de Presentes</h2>
         {listaDeCompras.map((item) => (
-          <ListaItens
-            key={item.id}
-            {...item}
-          />
+          <ListaItens key={item.id} {...item} />
         ))}
       </ol>
+      <button className={estilos.botoes__tipo__up} onClick={topo}>
+        <BsArrowUpCircleFill />
+        VOLTAR AO TOPO
+      </button>
     </div>
   );
 }
